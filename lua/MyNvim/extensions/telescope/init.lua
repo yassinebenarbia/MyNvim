79632e980d5fe2c0ea1d9d263228ab7ed8df13ec
yassinebenarbia <yassinebenarbia980@gmail.com>
2023-08-20 19:22:06 +0100

    imporved file regex, still working on the telescope extension

--- lua/MyNvim/extensions/telescope/init.lua
+++ lua/MyNvim/extensions/telescope/init.lua
@@ -1,7 +1,7 @@
 ---@breif [[
 --- under the telescope folder there write a file that have meaningfull name
 --- with respect to it's context and put this on it
----`require("telescope").load_extensioneturn(<file_name>)`
+---`require("telescope").load_extensione(<file_name>)`
 --- such that the <file_name> is file that is under the /lua/telescoe/_extensinons/
 --- with the exact same name, for exampe if the <file_name> is named neorg, thus the
 --- file should be named as neorg.lua
@@ -9,26 +9,41 @@
 ---@breif ]]
 --
 -- Base Path for all the plugins configs
-local plugins_base_path  = debug.getinfo(1).source:match("@?(.*/)")
-local file_name_command = string.format("ls -pa %s | grep -v /", plugins_base_path)
+local telescope_ext_base_path  = debug.getinfo(1).source:match("@?(.*/)")
+local file_name_command = string.format(
+  "ls -pa %s | grep -Po '.*?(?=.lua)'",
+  telescope_ext_base_path
+)
 local config_path = vim.fn.stdpath("config")
 
 -- ToDo: check for files and directories, if it's a file 
 -- then load it if it's a directory load the init.lua 
 -- just below it
 
+-- pcall(require, 'telescope.extensions')
+
 
 for file in io.popen(file_name_command):lines() do
 
-  if( file ~= "init.lua" ) then
+  if( file ~= "init" ) then
 
-    local link_file = string.format(
-      "echo ln -s %s/lua/MyNvim/extensions/telescope/%s \
-      $HOME/.local/share/nvim/lazy/telescope.nvim/lua/telescope/_extensions/%s | bash",
-      config_path ,file, file)
+    local state, telescope = pcall(require, 'telescope')
 
-    pcall(io.popen, link_file)
-    
+    if state then
+
+      local fstat, fcontent = pcall(require, 'MyNvim.extensions.telescope.' .. file)
+      if fstat then
+
+        for key, value in pairs(fcontent) do
+
+          telescope.extensions[key] = value
+
+        end
+
+      end
+
+    end
+
   end
 
 end
--- lua/MyNvim/extensions/telescope/neorg.lua
+++ lua/MyNvim/extensions/telescope/neorg.lua
@@ -1,10 +1,30 @@
-return require("telescope").register_extension {
+return{
 
-  setup = function(ext_config, config)
-    -- access extension config and user config
+  neorg = {
 
-  end,
-  exports = {
-    workspaces = "nothing",
+    setup = function(ext_config, config)
+      -- access extension config and user config
+    end,
+    exports = {
+      stuff = "t"
+    },
+
   },
+
+  xnorg = {
+
+    setup = function(ext_config, config)
+      -- access extension config and user config
+    end,
+
+    exports = {
+      stuff = function ()
+        print('dd')
+        print('dd')
+      end
+    },
+
+  },
+
 }
+
