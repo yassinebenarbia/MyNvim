e292ab36611a570e92bb59f30e84d57318b8d1fe
yassine <yassinebenarbia980@gmail.com>
2024-04-16 23:46:24 +0200

    chor: changed the default colorscheme

--- lua/plugins-install/colorschemes/tokyonight.lua
+++ lua/plugins-install/colorschemes/tokyonight.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/tokyonight.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	lazy = true, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		-- load the colorscheme here
