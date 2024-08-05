3289ba0ddf0856d28bba04ca0877773ac66fe491
Yassine <yassinebenarbia980@gmail.com>
2024-08-05 09:29:00 +0300

    fixed the <Undefined global 'vim'> warning

--- lua/MyNvim/plugins-config/lspconfig.lua
+++ lua/MyNvim/plugins-config/lspconfig.lua
@@ -5,7 +5,30 @@ return function(_, _)
 
   -- lspconfig.pyright.setup {}
   lspconfig.pylsp.setup {}
-  lspconfig.lua_ls.setup {}
+  lspconfig.lua_ls.setup {
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {
+            'vim',
+            'require'
+          },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  }
   lspconfig.tsserver.setup {}
   lspconfig.rust_analyzer.setup {
     -- Server-specific settings. See `:help lspconfig-setup`
