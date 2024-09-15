01b7cbb4f848709d5998c1b4a797ecc2fcec305d
Yassine <yassinebenarbia980@gmail.com>
2024-09-15 23:49:36 +0300

    fix: rool back to neorg 8.0.0 to make file/url links work properly

--- lua/plugins-install/neorg.lua
+++ lua/plugins-install/neorg.lua
@@ -3,6 +3,7 @@ local M = {
   "nvim-neorg/neorg",
   build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
+  version = "8.0.0";
   lazy = true,
   config = function()
     if status then
